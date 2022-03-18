d0cf2d71a9a2eb0f31e96f38085936bd825515eb
balaji <tbbalajibass10@gmail.com>
2022-03-18 11:42:27 +0530

    Apps.js changes

--- apps.js
+++ apps.js
@@ -1,17 +1,19 @@
 var express=require("express")
 var mongo=require("mongoose")
 var app=express();
-var body=require('body-parser');
+var bodyParser=require('body-parser');
 var cors=require('cors')
 var reg=require('./Routers/Register.js')
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors())
 var connectionString="mongodb://localhost:27017/Web-Deploy"
 mongo.connect(connectionString,(err)=>{
     if(err) throw err;
     console.log("DB is Connected...");
 })
-app.use(body.json())
+app.use(bodyParser.json())
 app.use('/api',reg);
 app.get('/',(req,res)=>{
     res.send("Welcome To Login Apps")
